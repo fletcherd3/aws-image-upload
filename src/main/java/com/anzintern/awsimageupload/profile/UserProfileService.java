02b9f9fe275f9ebd6bc25be6ec5a4a9e724f953d
Fletcher Dick <fletcher.dick@anz.com>
2021-02-03 09:41:27 +1300

    Creating guts of the User Profile service layer

--- src/main/java/com/anzintern/awsimageupload/profile/UserProfileService.java
+++ src/main/java/com/anzintern/awsimageupload/profile/UserProfileService.java
@@ -1,4 +1,27 @@
 package com.anzintern.awsimageupload.profile;
 
+import java.util.List;
+import java.util.UUID;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+@Service
 public class UserProfileService {
+
+	private final UserProfileDataAccessService userProfileDataAccessService;
+
+	@Autowired
+	public UserProfileService(
+			UserProfileDataAccessService userProfileDataAccessService) {
+		this.userProfileDataAccessService = userProfileDataAccessService;
+	}
+
+	List<UserProfile> getUserProfiles() {
+		return userProfileDataAccessService.getUserProfiles();
+	}
+
+	public void uploadUserProfileImage(UUID userProfileId, MultipartFile file) {
+	}
 }
