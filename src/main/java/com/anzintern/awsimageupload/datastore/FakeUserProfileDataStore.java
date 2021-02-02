ac3c9932071b3e37d2309135de203b3f7bd2f7b2
Fletcher Dick <fletcher.dick@anz.com>
2021-02-02 16:25:49 +1300

    Created static mock data object and made profile image links optional

---
+++ src/main/java/com/anzintern/awsimageupload/datastore/FakeUserProfileDataStore.java
@@ -0,0 +1,26 @@
+package com.anzintern.awsimageupload.datastore;
+
+import java.nio.file.attribute.UserPrincipal;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
+import org.springframework.stereotype.Repository;
+
+import com.anzintern.awsimageupload.profile.UserProfile;
+
+@Repository
+public class FakeUserProfileDataStore {
+
+	private static final List<UserProfile> USER_PROFILES = new ArrayList<>();
+
+	static {
+		USER_PROFILES.add(new UserProfile(UUID.randomUUID(), "fletcher_dick", null));
+		USER_PROFILES.add(new UserProfile(UUID.randomUUID(), "josh_gomez", null));
+		USER_PROFILES.add(new UserProfile(UUID.randomUUID(), "vasili_aliashkevich", null));
+	}
+
+	public List<UserProfile> getUserProfiles() {
+		return USER_PROFILES;
+	}
+}
--- src/main/java/com/anzintern/awsimageupload/profile/UserProfile.java
+++ src/main/java/com/anzintern/awsimageupload/profile/UserProfile.java
@@ -1,6 +1,7 @@
 package com.anzintern.awsimageupload.profile;
 
 import java.util.Objects;
+import java.util.Optional;
 import java.util.UUID;
 
 public class UserProfile {
@@ -50,8 +51,8 @@ public class UserProfile {
 		return userName;
 	}
 
-	public String getUserProfileImageLink() {
-		return userProfileImageLink;
+	public Optional<String> getUserProfileImageLink() {
+		return Optional.ofNullable(userProfileImageLink);
 	}
 
 	public void setUserProfileImageLink(String userProfileImageLink) {
